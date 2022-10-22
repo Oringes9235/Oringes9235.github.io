c8ddc25b2b9b076fbbfa72e84ab7756938b0e5d2
Oringe <x92352314@163.com>
2022-10-22 19:35:41 +0800

    [add]

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oringe9235.github.io
+
+it only a web code lib.
---
+++ submit.sh
@@ -0,0 +1,7 @@
+ssh -T git@github.com
+
+git remote add origin git@github.com:Oringes9235/Oringes9235.github.io.git
+
+git push -u origin master
+
+pause
